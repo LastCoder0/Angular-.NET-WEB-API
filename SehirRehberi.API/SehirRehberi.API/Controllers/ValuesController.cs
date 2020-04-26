d889606044a864c04989f025e2d1ea97bd36bbe6
LastCoder0 <37277772+LastCoder0@users.noreply.github.com>
2020-04-27 01:30:49 +0300

    Calismam

---
+++ SehirRehberi.API/SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SehirRehberi.API.Data;
+
+namespace SehirRehberi.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+       private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            //injekte ediyoruz
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v=>v.Id==id);
+            return Ok(value);
+        }
+       
+
+    }
+}
---
+++ SehirRehberi.API/SehirRehberi.API/Models/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Models
+{
+    public class City
+    {
+        public City()
+        {
+            photos = new List<Photo>();
+        }
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Photo> photos { get; set; }
+        public User User { get; set; }
+
+    }
+}
